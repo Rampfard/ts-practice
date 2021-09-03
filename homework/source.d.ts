f780aa173c7840e66426ecd614660296b57523a4
Roman <R.BlackNet@gmail.com>
2021-09-03 17:07:46 +0300

    прокомментировал source.d.ts

--- homework/source.d.ts
+++ homework/source.d.ts
@@ -8,19 +8,43 @@ export interface Defaults {
 	pattern?: string;
 	negativePattern?: string;
 }
-
+/* 
+	Вот тут вопрос про огород из ? (необязательных свойств).
+	Подумал что если мы хотим пердать в функцию свои настройки, допустим только разделитель(separator),
+	то придётся писать ВСЕ настройки при передаче, даже те что остаются дефолтными, а их как видно дохрена,
+	поэтому пометил всё как необязательное. Внутри библиотеки этот объект всё равно берётся из окружения.
+	Передавать его не обязательно, а если разрешить передачу только Settings что описаны ниже,
+	мы не сможем менять дефолтные и поэтому всё же стоит их оставить.
+*/
 export interface Settings extends Defaults {
 	useVedic?: boolean;
 	increment?: number;
 	groups?: RegExp;
 }
 
+// Эта строчка в принципе не нужна, функции основанные на этом типе используются внутри библиотеки.
+// Библиотека не будет смотреть на этот файл d.ts так что это просто лишний код. Сделал для лёгкой практики.
 export type NumberFn = (v: number, i?: number) => number;
 
+/* 
+	Описал классом для удобства.
+	Добавил readonly и private поля для того чтобы в тс файле их нельзя было изменить у объекта.
+	Не совсем понял предполагалось ли библиотекой value менять напрямую,
+	но методы add, multiply явно для его изменения внутри.
+	Приватные для того чтобы их вообще нельзя было получить. Насколько мне известно,
+	свойства что начинаются с _ предназначены для внутреннего использования, 
+	логично что неплохо бы пометить как приватные.
+	В JS такого не будет, но мы и весь код в TS пишем изначально.
+ */
 export default class Currency {
 	readonly value: number | string | Currency;
 	readonly opts: Settings;
-	private _settings: Settings;
+
+	// Внутри фукнции currency defaults объект и setting сливаются в один _settings.
+	// Логично что в settings все поля из defaults тоже присутствуют.
+	// В методах как раз и передаётся _settings в currency. Поэтому ожидать на вход только Settings 
+	// должно выдавать ошибку типа так как позже в currency внутри методов приходят settings (объединённые с deafults) 
+	private _settings: Settings; 
 	private _precision: number;
 
 	constructor(value: number | string | Currency, opts?: Settings) {
@@ -34,6 +58,8 @@ export default class Currency {
 
 	multiply(number: number): Currency;
 
+	// Вот тут не ясно, в jsdocs к библиотеке в return указано что возвращается массив Currency экземпляров.
+	// Да и в консоле там масив из экземпляров Currency, поэтому возвращаемое значение указано как Currency[]; 
 	divide(number): Currency[];
 
 	distribute(count: number): Currency[];
@@ -46,5 +72,8 @@ export default class Currency {
 
 	toString(): string;
 
+
+	// number в JSON. Зачем не понятно, логично же что тут ожидается операция JSON.stringify() которая вернёт строку.
+	// Но автор указал что возвращается number. А именно this.value что и есть number.
 	toJSON(): number;
 }

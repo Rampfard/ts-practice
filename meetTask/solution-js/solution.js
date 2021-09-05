0843d69111474bcee514f13ad925b6d0ccddade5
Roman <R.BlackNet@gmail.com>
2021-09-05 19:01:49 +0300

    compile js

--- meetTask/solution-js/solution.js
+++ meetTask/solution-js/solution.js
@@ -3,39 +3,35 @@ const groupBy = (arr, cb) => {
     const obj = {};
     for (let item of arr) {
         const key = cb(item);
-        obj[key] = [...arr.filter((el) => cb(el) === key)];
+        obj[key] = arr.filter((el) => cb(el) === key);
     }
     return obj;
 };
-// console.log(groupBy([1.2, 1.1, 2.3, 0.4], Math.floor));
-// let result = {
-// 	'0': [0.4],
-// 	'1': [1.2, 1.1],
-// 	'2': [2.3],
-// };
-// console.log(groupBy(['one', 'two', 'three'], (el) => el.length));
-// let result2 = {
-// 	'3': ['one', 'two'],
-// 	'5': ['three'],
-// };
-// enum Gender {
-// 	Male,
-// 	Female,
-// }
-// console.log(
-// 	groupBy(
-// 		[
-// 			{ g: Gender.Male, n: 'A' },
-// 			{ g: Gender.Female, n: 'B' },
-// 			{ g: Gender.Female, n: 'C' },
-// 		],
-// 		(el) => el.g
-// 	)
-// );
-// let result3 = {
-//   [Gender.Male]: [{ g: Gender.Male, n: "A" }],
-//   [Gender.Female]: [
-//     { g: Gender.Female, n: "B" },
-//     { g: Gender.Female, n: "C" },
-//   ],
-// };
+console.log(groupBy([1.2, 1.1, 2.3, 0.4], Math.floor));
+let result = {
+    '0': [0.4],
+    '1': [1.2, 1.1],
+    '2': [2.3],
+};
+console.log(groupBy(['one', 'two', 'three'], (el) => el.length));
+let result2 = {
+    '3': ['one', 'two'],
+    '5': ['three'],
+};
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+})(Gender || (Gender = {}));
+console.log(groupBy([
+    { g: Gender.Male, n: 'A' },
+    { g: Gender.Female, n: 'B' },
+    { g: Gender.Female, n: 'C' },
+], (el) => el.g));
+let result3 = {
+    [Gender.Male]: [{ g: Gender.Male, n: 'A' }],
+    [Gender.Female]: [
+        { g: Gender.Female, n: 'B' },
+        { g: Gender.Female, n: 'C' },
+    ],
+};
